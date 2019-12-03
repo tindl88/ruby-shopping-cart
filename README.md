f4d7815b10ea3175e1a731d53fbc98bbcdb5c080
Tin Tran <tindl88@gmail.com>
2019-12-03 12:42:11 +0700

    no message

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ruby-shopping-cart
+
+Run: `rubymon ./app.rb`
+Test: `rspec ./spec/test_case.rb`
---
+++ spec/test_case.rb
@@ -0,0 +1,13 @@
+describe 'Input' do
+  it 'des' do
+    ordered_1 = 1
+    expect(ordered_1).to eq(1)
+  end
+end
+
+describe 'Onput' do
+  it 'des2' do
+    int_0 = 0
+    expect(int_0).to eq(0)
+  end
+end
